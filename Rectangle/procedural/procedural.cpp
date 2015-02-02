8e92ad90558adf9eff78fae01a9465fa7de3ecbd
Austin <hillam.mail@gmail.com>
2015-02-01 20:09:58 -0500

    Fixed Rectangle procedural

--- Rectangle/procedural/procedural.cpp
+++ Rectangle/procedural/procedural.cpp
@@ -25,5 +25,7 @@ int perimeter(int w,int h){
 bool contains(int x1,int y1,int w1,int h1,
 			  int x2,int y2,int w2,int h2){
 	return (x2 > x1 && y2 > y1 && 
-			w2 < w1 && h2 < h1);
+			w2 < w1 && h2 < h1 &&
+			(x2 + w2) < (x1 + w1) &&
+			(y2 + h2) < (y1 + h1));
 }
